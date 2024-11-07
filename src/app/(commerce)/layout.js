63509db8da7d945e1095258ff785fe95115a7f08
Tapas Adhikary <tapas.adhikary@gmail.com>
2024-11-07 14:35:34 +0530

    (fix) With the upgraded version of Next.js(14.2.17) it is mandatory to have the <html> and <body> tags forall layout.

--- src/app/(commerce)/layout.js
+++ src/app/(commerce)/layout.js
@@ -9,5 +9,5 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
-  return <div className="w-full h-screen flex flex-col">{children}</div>;
+  return <html><body><div className="w-full h-screen flex flex-col">{children}</div></body></html>;
 }
